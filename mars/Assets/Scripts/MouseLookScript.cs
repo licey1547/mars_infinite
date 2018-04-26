6a040385c422c55f733263e9d4272f7ae29a5e9f
Nikolay <kolia.ksenofontov@yandex.ru>
2018-04-26 22:14:05 +0300

    .

---
+++ mars/Assets/Scripts/MouseLookScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLookScript : MonoBehaviour {
+
+    [HideInInspector]
+    public float xRotation;
+    [HideInInspector]
+    public float yRotation;
+    public float lookSense = 5;
+    [HideInInspector]
+    public float currXRotation;
+    [HideInInspector]
+    public float currYRotation;
+    [HideInInspector]
+    public float xRotationVelocity;
+    [HideInInspector]
+    public float yRotationVelocity;
+    public float smoothDampTime = 0.1f;
+
+    void Update() {
+
+        xRotation -= Input.GetAxis("Mouse Y") * lookSense;
+        yRotation += Input.GetAxis("Mouse X") * lookSense;
+
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        currXRotation = Mathf.SmoothDamp(currXRotation, xRotation, ref xRotationVelocity, smoothDampTime);
+        currYRotation = Mathf.SmoothDamp(currYRotation, yRotation, ref yRotationVelocity, smoothDampTime);
+
+        transform.rotation = Quaternion.Euler(currXRotation, currYRotation, 0);
+    }
+}
---
+++ mars/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+	public GameObject o;
+	public float moveSpeed = 4f;
+
+	void Update () {
+		if(Input.GetKey(KeyCode.W)){
+			o.transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.S)){
+			o.transform.Translate (-Vector3.forward * moveSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.A)){
+			o.transform.Translate (new Vector3(-1f, 0, 0) * moveSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.D)){
+			o.transform.Translate (new Vector3(1f, 0, 0) * moveSpeed * Time.deltaTime);
+		}
+	}
+}
